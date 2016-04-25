09de245529933c13de0747de4b47e5f04bf49898
Asoul <azx754@gmail.com>
2016-04-25 23:47:41 +0800

    AX-14, Fix top bar click change background color

--- themes/apollo/source/js/script.js
+++ themes/apollo/source/js/script.js
@@ -19,7 +19,7 @@
   }
 
   // Mobile nav
-  $(".mobile-nav-panel, .nav-cover, .nav-close").on("click", function(e) {
+  $(".menu-bar-icon, .nav-cover, .nav-close").on("click", function(e) {
       e.preventDefault()
       $("html").toggleClass("nav-opened nav-closed")
   })
